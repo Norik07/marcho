6590bec8d8d812f9cadd951985ceee9d1021c955
Norik07 <babayannorik777@gmail.com>
2023-06-23 15:54:34 +0400

    added product section

--- gulpfile.js
+++ gulpfile.js
@@ -60,6 +60,7 @@ function scripts() {
   return src([
     "node_modules/slick-carousel/slick/slick.js",
     "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+    "node_modules/rateyo/src/jquery.rateyo.js",
     "app/js/main.js",
   ])
     .pipe(concat("main.min.js"))
